314e03b03cd0b267f1bdd17a7ca2451aa197a91e
geddann2 <geddann2@gmail.com>
2025-11-13 10:10:05 +0900

    middleware.js update

--- middleware.js
+++ middleware.js
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server"
 
-export async function middleware(){
-    console.log("ミドルウェア")
-    return NextResponse.next()
+export async function middleware(request){
+    const token = request.headers.get("Authorization")?.split(" ")[1]
+    if(!token){
+        return NextResponse.json({message:"トークンがありません"})
+    }
+    return NextResponse.next() 
 }
 export const config
- = {
-    matcher:["/api/item/create","/api/item/update/:path*","/api/item/delete/:path*"],
+ = {matcher:["/api/item/create","/api/item/update/:path*","/api/item/delete/:path*"],
 }
