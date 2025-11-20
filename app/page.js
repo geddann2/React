e558f5c0715fb431fc2c8f42fe6b362515996f11
geddann2 <geddann2@gmail.com>
2025-11-20 11:38:32 +0900

    add register

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 const ReadAllItems = () => {
     return(
         <div>
-            <h1>こんにちは</h1>
-            <h3>さようなら</h3>
+            <h1 className = "h1-style">こんにちは</h1>
+            <h3 style={{marginTop:"50px"}}>さようなら</h3>
         </div>
     )
 }
